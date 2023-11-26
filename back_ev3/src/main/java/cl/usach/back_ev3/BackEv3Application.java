8b1bee6bc7ec45b9c0993aaac5e15504e57f9b6b
FranciscaCMG <francisca.marquez@usach.cl>
2023-11-26 13:29:31 -0300

    creacion back y front

---
+++ back_ev3/src/main/java/cl/usach/back_ev3/BackEv3Application.java
@@ -0,0 +1,13 @@
+package cl.usach.back_ev3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEv3Application.class, args);
+	}
+
+}
---
+++ back_ev3/src/test/java/cl/usach/back_ev3/BackEv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usach.back_ev3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
